9724d73b2b35519e7aa60d868822b541b539c615
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-05-09 19:57:18 +0530

    starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { marked } from "marked";
+
+import { useState } from "react";
+import { mangle } from "marked-mangle";
+import DOMPurify from "dompurify";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     const [text, setText] = useState("");
+     function handleChange(e) {
+          setText(e.target.value);
+     }
+     marked.use(mangle());
+     const renderChange = (text) => {
+          const html = marked.parse(text);
+          let __html = DOMPurify.sanitize(html);
+          return { __html };
+     };
+     return (
+          <div className="container">
+               <div className="row">
+                    <div className="col-sm-6">
+                         <textarea
+                              className="form-control"
+                              rows="35"
+                              onChange={handleChange}
+                              value={text}
+                              placeholder="let's start writing ..."
+                         />
+                         {/* input */}
+                    </div>
+                    <div className="col-sm-6">
+                         <div dangerouslySetInnerHTML={renderChange(text)} />
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default App;
